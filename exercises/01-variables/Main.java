2650b89c2d7cadc48ca4be08038fdbc5666c04dc
Renata Honorato <renatahonorato2023@hotmail.com>
2026-08-07 19:46:09 -0300

    feat: add variables exercise

--- exercises/01-variables/Main.java
+++ exercises/01-variables/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        
+
+        String nome = "Barão";
+        int idade = 9;
+        double peso = 320.4;
+        char inicial = 'B';
+        boolean estaSaudavel = true;
+        byte alimentacoesNoDia = 2;
+
+        System.out.println(nome);
+        System.out.println(idade);
+        System.out.println(peso);
+        System.out.println(inicial);
+        System.out.println(estaSaudavel);
+        System.out.println(alimentacoesNoDia);
+
     }
 }
